c1d86d314d31549eb75b9b0dbdce3817da09c41b
Yuheng Zheng <swanboiled@gmail.com>
2016-03-01 19:09:00 +0800

    use /static/media for media root

--- wsgi/ba/ba/settings.py
+++ wsgi/ba/ba/settings.py
@@ -137,5 +137,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(WSGI_DIR, 'media')
+MEDIA_ROOT = os.path.join(WSGI_DIR, 'static/media')
 MEDIA_URL='/media/'
